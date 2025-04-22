163ce272d2533efa6050beeea6066ff4d17723cb
Axaweb <axaweb@mail.ru>
2025-04-22 09:48:31 +0200

    5-JavaScript-TypeScript-Начало-Переменные

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  headerTop1Item1 = "Главная";
+  headerTop1Item2 = "О компании";
+  headerTop1Item3 = "Каталог";
+
+  headerTop2Item1 = "Каталог";
+  headerTop2Item2 = "Стройматериалы";
+  headerTop2Item3 = "Инструменты";
+  headerTop2Item4 = "Электрика";
+  headerTop2Item5 = "Интерьер и одежда";
 }
